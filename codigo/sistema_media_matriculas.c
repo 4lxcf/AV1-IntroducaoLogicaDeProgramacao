ed1443689bb4ae8dd4947b27b60b64c76945694f
ALEXANDRE FERREIRA <56090159+4lxcf@users.noreply.github.com>
2026-04-16 21:25:48 -0300

    Update sistema_media_matriculas.c

--- codigo/sistema_media_matriculas.c
+++ codigo/sistema_media_matriculas.c
@@ -1,28 +1,55 @@
+/*
+O programa a ser desenvolvido em linguagem C devera apresentar um menu principal que permaneca em execucao contınua ate que o usuario digite S ou s para encerrar o sistema. No interior do laco de repeticao, o sistema devera solicitar as seguintes informacoes:
+• Nome do aluno;
+• Documento de identificacao;
+• Numero de matrıcula.
+Em seguida, o programa devera receber as disciplinas cursadas pelo aluno, contendo:
+• Nome da disciplina;
+• Media obtida na disciplina.
+A entrada das disciplinas e respectivas notas devera ocorrer de forma contınua, sendo finalizada quando for informada uma nota negativa, a qual funcionara como criterio de parada. Durante o processo de insercao das notas, o programa devera:
+• Acumular a soma das medias informadas;
+• Contabilizar a quantidade de disciplinas registradas;
+• Calcular a media final do aluno ao termino da entrada de dados.
+Apos a insercao de uma nota negativa, o sistema devera exibir a media final das disciplinas cursadas e proceder a seguinte verificacao:
+• Caso a media seja maior ou igual a 7,0, o aluno devera ser considerado aprovado, podendo escolher entre cinco novas disciplinas que deverao ser apresentadas pelo sistema;
+• Caso a media seja inferior a 7,0, o aluno devera ser considerado reprovado. Nesse caso, o sistema devera questionar se o aluno deseja repetir a disciplina:
+– Em caso afirmativo, o aluno devera escolher entre quatro novas disciplinas;
+– Em caso negativo, o aluno devera escolher entre cinco disciplinas distintas daquelas oferecidas aos alunos aprovados.
+Adicionalmente, as estruturas utilizadas no desenvolvimento do programa deverao estar em conformidade com os conteudos apresentados em sala de aula, contemplando o uso adequado de variaveis, estruturas de decisao, estruturas de repeticao e organizacao logica do algoritmo.
+*/
+
 #include <stdio.h>
 
 int main(){
-  char exec = 'E', response;
+  char exec, response;
   char studentName[20], subject[12];
   int docId, studentId, subjectCount = 0;
   float subjectAverage = 0., totalAverage = 0., averageSum = 0.;
 
-  while (exec != 'S' || exec != 's'){
+  do
+  {
     printf("#################################################\n");
     printf("SEJA BEM-VINDO AO SISTEMAS DE MEDIAS E MATRICULAS\n");
     printf("#################################################\n\n");
-
-    printf("Para iniciar, digite o nome do aluno:\n");
-    scanf("%s", &studentName);
+    printf("Deseja iniciar um novo aluno? Digite C para continuar e S para sair.\n");
+    scanf("%c", &exec);
+    if (exec == 'S' || exec == 's')
+    {
+      break;
+    }
+    
+    printf("Para iniciar, digite o Nome do Aluno:\n");
+    scanf(" %s", &studentName);
     printf("\nAgora digite o numero do Documento de Identificacao do aluno (somente numeros):\n");
     scanf(" %d", &docId);
-    printf("\nPor fim, digite o numero de matricula do aluno (somente numeros):\n");
+    printf("\nPor fim, digite o Numero de Matricula do aluno (somente numeros):\n");
     scanf(" %d", &studentId);
 
     do
     {
-      printf("\nOK! Agora insira o nome da disciplina cursada:\n");
+      printf("\nInsira o nome da Disciplina cursada:\n");
       scanf("%s", &subject);
-      printf("\nInsira a media do aluno nesta disciplina:\n");
+      printf("\nInsira a Media do aluno nesta Disciplina:\n");
       scanf(" %f", &subjectAverage);
 
       if(subjectAverage >= 0){
@@ -46,11 +73,7 @@ int main(){
         printf("\n\nEscolha uma das 5 disciplinas para cursar:\n1-DISCIPLINA ALTERNATIVA I\n2-DISCIPLINA ALTERNATIVA II\n3-DISCIPLINA ALTERNATIVA III\n4-DISCIPLINA ALTERNATIVA IV\n5-DISCIPLINA ALTERNATIVA V\n");
       }
     }
-
-    exec = 'S';
-    printf(" %c\n", exec);
-
-  }
+  } while (exec != 'S' || exec != 's');
 
   return 0;
 }
